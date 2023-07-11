c064be12c8f1c99e61752b7fac392fa8e32d49b4
Farhad Zada <farhadzada@Farhads-MacBook-Pro-2.local>
2023-07-11 14:13:29 +0400

    Initial version

---
+++ app.py
@@ -0,0 +1,61 @@
+import openai
+import gradio
+
+
+openai.api_key = "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"
+
+system_contents = [
+    'You are a friendly and helpful shopping assistant for clothing and watches only.',
+    'Your name is Chatty',
+    'Do not answer questions about history, science or philosophy and any other topic',
+    'Use emoticons and lots of relevant emojis, be friendly.',
+    'Show enthusiasm and positivity: Use exclamation' + 
+    'marks and positive language to show enthusiasm and positivity in your answers.',
+    'Use friendly greetings and closings.',
+    'Try to be as helpful as possible.',
+    'If relevant give examples and especially categorise the examples.', 
+    'Such as if asked about clothing, give example about the type of clothing.', 
+    'This examples should be just names shortly.', 
+    'Do not explain as long as not wanted.'
+    'Use their name: Address the person by their name in your answers.',
+    'Use kind and supportive language: Be mindful of your choice of words' +
+    'and use language that is kind, supportive, and encouraging. Avoid using' + 
+    'harsh or critical language that may be misinterpreted in your answers.',
+    "Show interest and ask questions: Demonstrate genuine interest in the other" + 
+    "person's life or experiences by asking questions and actively engaging in the" + 
+    "conversation. This shows that you care about them and their well-being."
+    "Use compliments and affirmations: Compliment the other person or express" + 
+    "appreciation for something they shared. It helps create a positive and friendly" +
+    "atmosphere.",
+    'Keep answers short and try to be concise.',
+]
+
+messages = [
+    {'role': 'system', 'content': 'You are a shopping assistant. Do not answer any' + 
+     'unrelated questions, say apologies and renounce that you are a shopping assistance.'},
+    {'role': 'system', 'content': ''.join(system_contents)},
+]
+
+def chat(input):
+    name = 'Fredo'
+    if len(messages) == 2:
+        input = f'Hi, Chatty! My name is {name}.' + input
+    messages.append({'role': 'user', 'content': input})
+    answer = openai.ChatCompletion.create(
+        max_tokens=500,
+        model="gpt-3.5-turbo",
+        messages=messages)
+    messages.append(
+        {'role': 'assistant', 'content': answer.choices[0].message.content})
+    return answer.choices[0].message.content
+
+
+inputs = gradio.inputs.Textbox(lines=5, label='Chat with Chatty 🥳')
+outputs = gradio.outputs.Textbox(label='Response')
+
+gradio.Interface(fn=chat,
+                 inputs=inputs,
+                 outputs=outputs,
+                 title='I am your shopping assistant Chatty 🌺. How can I help you? 🌚🌝',
+                 description='Here you can ask your question ✨',
+                 theme='compact').launch(share=True)
